2419ffee8fff0fcfd56bbb3ede15d67fc8f6672c
OlaBadr <olabadr.elgedai@gmail.com>
2022-08-09 10:22:50 +0200

    application of MC1

---
+++ echo1/Debug/echo1.d
@@ -0,0 +1,7 @@
+echo1.o echo1.o: ../echo1.c ../std_types.h ../common_macros.h ../usart.h
+
+../std_types.h:
+
+../common_macros.h:
+
+../usart.h:
---
+++ echo2/Debug/echo2.d
@@ -0,0 +1,7 @@
+echo2.o echo2.o: ../echo2.c ../std_types.h ../common_macros.h ../usart.h
+
+../std_types.h:
+
+../common_macros.h:
+
+../usart.h:
