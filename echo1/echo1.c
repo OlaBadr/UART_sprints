ad7a7a6a5842902ebd42dccce2e54cfb204d520d
OlaBadr <olabadr.elgedai@gmail.com>
2022-08-09 19:31:53 +0200

    done

--- echo1/echo1.c
+++ echo1/echo1.c
@@ -21,15 +21,24 @@
 int main ()
 {
 
+	SWUART_init(9600);
 	/*add delay before starting the communication*/
 	_delay_ms(500);
-
+	uint8_t sent_data = 16;
+	uint8_t recieved_data;
 
 	while(1)
 	{
+		SWUART_send(sent_data); //echo
+		_delay_ms(100);
+		SWUART_recieve(&recieved_data); //echo back
+		_delay_ms(100);
 
+		if (sent_data == recieved_data)
+			continue;
 
-
+		else
+			break;
 	}
 
 	return 0;
