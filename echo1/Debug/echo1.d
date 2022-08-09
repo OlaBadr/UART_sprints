cc0de7ca68d5c193bfba85b7cf5dc17759ced0f9
OlaBadr <olabadr.elgedai@gmail.com>
2022-08-09 10:25:45 +0200

    Revert "application of MC1"
    This reverts commit 2419ffee8fff0fcfd56bbb3ede15d67fc8f6672c.

--- echo1/Debug/echo1.d
+++
@@ -1,7 +0,0 @@
-echo1.o echo1.o: ../echo1.c ../std_types.h ../common_macros.h ../usart.h
-
-../std_types.h:
-
-../common_macros.h:
-
-../usart.h:
--- echo2/Debug/echo2.d
+++
@@ -1,7 +0,0 @@
-echo2.o echo2.o: ../echo2.c ../std_types.h ../common_macros.h ../usart.h
-
-../std_types.h:
-
-../common_macros.h:
-
-../usart.h:
